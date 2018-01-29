c1a7b0c85215f0ad72e6da5cc8402e0d24c4d8e7
David Gibson <dgibson314@gmail.com>
2018-01-29 11:30:11 -0500

    Making display update more modular

--- calc.js
+++ calc.js
@@ -1,36 +1,41 @@
+function updateDisplay(button) {
+    var buttonVal = $(button).text();
+    var buttonType = $(button).attr("class");
+
+    switch (buttonType) {
+        // AC or CE
+        case "special-btn":
+            if (buttonVal === "AC") {
+                $("#current-val").text("0");
+                $("#op-chain").text("0");
+            }
+            break;
+
+        // Operator
+        case "op-btn":
+            break;
+
+        // Number
+        case "num-btn":
+            var currValDisplay = $("#current-val").text();
+            if (currValDisplay === "0") {
+                if (buttonVal !== "0") {
+                    $("#current-val").text(buttonVal);
+                }
+                else {}
+            }
+            else {
+                $("#current-val").append(buttonVal);
+            }
+            break;
+    }
+
+}
+
 $(document).ready(function() {
     
-    $("button").click(function() {
-        var buttonVal = $(this).text();
-        switch ($(this).attr("class")) {
-            // AC or CE
-            case "special-btn":
-                if (buttonVal === "AC") {
-                    $("#current-val").text("0");
-                    $("#op-chain").text("0");
-                }
-                break;
-
-            // Operator
-            case "op-btn":
-                break;
-
-            // Number
-            case "num-btn":
-                var currVal = $("#current-val").text();
-                if (currVal === "0") {
-                    if (buttonVal !== "0") {
-                        $("#current-val").text(buttonVal);
-                    }
-                    else {
-                        ;
-                    }
-                }
-                else {
-                    $("#current-val").append(buttonVal);
-                }
-                break;
-        }
+    $("button").on("click", function() {
+        updateDisplay(this);
     });
 
 });
